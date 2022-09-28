eafbe656226797bd57b478b1960ad4ca12973337
Daniel Haß <danielhass@users.noreply.github.com>
2022-09-28 16:32:02 +0200

    improved deploy-target.sh

--- scripts/deploy-target.sh
+++ scripts/deploy-target.sh
@@ -3,6 +3,9 @@
 # kontinuum-controller target deploy script
 # needs to be called from repository root
 
+# variables
+K8S_NAMESPACE=kontinuum-controller
+
 # check if Flux CLI is installed
 if ! command -v flux &>/dev/null; then
     echo "ERROR: flux is not installed; please visit https://fluxcd.io/flux/cmd/" 1>&2
@@ -17,13 +20,13 @@ echo "> Install necessary Flux components on the target cluster"
 flux install --components="source-controller,kustomize-controller,helm-controller"
 
 echo "> Create namespace for kontinuum-controller resources"
-kubectl create ns kontinuum-controller --dry-run=client -o yaml | kubectl apply -f -
+kubectl create ns ${K8S_NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
 
 echo -n "> Please enter the Helm repository URL: "
 read HELM_URL
 
 echo "> Creating Flux Helm source on target cluster: "
-flux create -n kontinuum-controller source helm kontinuum-catalog --interval=1h --url=${HELM_URL}
+flux create -n ${K8S_NAMESPACE} source helm kontinuum-catalog --interval=1h --url=${HELM_URL}
 
 echo -n "> Please enter the S3 endpoint URL: "
 read S3_URL
@@ -38,13 +41,13 @@ echo -n "> Please enter the S3 secret key: "
 read S3_SECRET_KEY
 
 echo "> Create Flux S3 bucket reference on target cluster: "
-flux create source bucket ${S3_BUCKET} --bucket-name=${S3_BUCKET} --endpoint=${S3_URL} --provider=generic --interval=1m --access-key=${S3_ACCESS_KEY} --secret-key=${S3_SECRET_KEY}
+flux create -n ${K8S_NAMESPACE} source bucket ${S3_BUCKET} --bucket-name=${S3_BUCKET} --endpoint=${S3_URL} --provider=generic --interval=1m --access-key=${S3_ACCESS_KEY} --secret-key=${S3_SECRET_KEY}
 
 echo -n "> Please enter the new target cluster name (used to reference it on the control-plane): "
 read TARGET_NAME
 
 echo "> Create Flux S3 bucket reference on target cluster: "
-flux create kustomization ${S3_BUCKET} --source=Bucket/${S3_BUCKET} --path="./${TARGET_NAME}" --interval=1m --prune
+flux create -n ${K8S_NAMESPACE} kustomization ${S3_BUCKET} --source=Bucket/${S3_BUCKET} --path="./${TARGET_NAME}" --interval=1m --prune
 
 if [ "$?" -ne 0 ]; then
     echo "> The last step might display an error 'kustomization path not found'. 
